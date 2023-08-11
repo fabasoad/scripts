a86779cb687dd21ee3bb49886d56131d6786220f
fabasoad <fabasoad@gmail.com>
2023-08-11 15:11:09 +0900

    Trim trailing whitespaces

--- src/organize.sh
+++ src/organize.sh
@@ -11,22 +11,22 @@ for f in "${src}"/*
 do
   if [ ! -d "$f" ]; then
     full_date=$(GetFileInfo -d "$f")
-  
+
     creation_date=$(echo "${full_date}" | cut -d' ' -f 1)
     IFS='/' read -r -a creation_date_arr <<< "${creation_date}"
     year=${creation_date_arr[2]}
     month=${creation_date_arr[0]}
     day=${creation_date_arr[1]}
-  
+
     creation_time=$(echo "${full_date}" | cut -d' ' -f 2)
     IFS=':' read -r -a creation_time_arr <<< "${creation_time}"
     hr=${creation_time_arr[0]}
     min=${creation_time_arr[1]}
     sec=${creation_time_arr[2]}
-  
+
     old_filename=$(basename -- "$f")
     ext="${old_filename##*.}"
-  
+
     new_folder="${year}.${month}.${day}"
     mkdir -p "${src}/${new_folder}"
     new_filename="${year}-${month}-${day}_${hr}-${min}-${sec}.${ext}"

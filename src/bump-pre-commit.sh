0eb123e3a270e7cfad03d3513eecc0f35f14c167
fabasoad <fabasoad@gmail.com>
2025-02-06 22:53:56 +0900

    Test 16

--- src/bump-pre-commit.sh
+++ src/bump-pre-commit.sh
@@ -7,26 +7,25 @@ LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
 . "${LIB_DIR_PATH}/logging.sh"
 
 main() {
-  cat README.md | head -n 1
-#  if [ -f ".pre-commit-config.yaml" ]; then
-#    pre-commit autoupdate
-#
-#    set +e
-#    git diff --quiet
-#    exit_code=$?
-#    set -e
-#
-#    if [ "${exit_code}" -eq 1 ]; then
-#      git config url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
-#      git add .
-#      git commit -m "chore: bump pre-commit hooks to the latest version"
-#      git push origin main
-#    else
-#      log_info "No changes in pre-commit hooks"
-#    fi
-#  else
-#    log_info ".pre-commit-config.yaml file is not found"
-#  fi
+  if [ -f ".pre-commit-config.yaml" ]; then
+    pre-commit autoupdate
+
+    set +e
+    git diff --quiet
+    exit_code=$?
+    set -e
+
+    if [ "${exit_code}" -eq 1 ]; then
+      git config url."https://${1}@github.com/".insteadOf "https://github.com/"
+      git add .
+      git commit -m "chore: bump pre-commit hooks to the latest version"
+      git push origin main
+    else
+      log_info "No changes in pre-commit hooks"
+    fi
+  else
+    log_info ".pre-commit-config.yaml file is not found"
+  fi
 }
 
 main "$@"

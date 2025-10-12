3a8986e316ca3816c221dd1876b6e02cdb6a6ac4
fabasoad <fabasoad@gmail.com>
2025-10-12 21:21:26 +0900

    ci: use github.token

--- src/automation/main.sh
+++ src/automation/main.sh
@@ -8,11 +8,12 @@ LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
 . "${LIB_DIR_PATH}/logging.sh"
 
 setup_git_config() {
-  git_user_name="${1}"
-  git_user_email="${2}"
+  token="${1}"
+  git_user_name="${2}"
+  git_user_email="${3}"
 
   log_info "Setting up git config started."
-  git config url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
+  git config url."https://${token}@github.com/".insteadOf "https://github.com/"
   git config user.name "${git_user_name}"
   git config user.email "${git_user_email}"
   log_info "Setting up git config completed."
@@ -26,11 +27,12 @@ run_scripts() {
 }
 
 main() {
-  git_user_name="${1}"
-  git_user_email="${2}"
+  token="${1}"
+  git_user_name="${2}"
+  git_user_email="${3}"
   echo "Directory 2: $(pwd)"
 
-  setup_git_config "${git_user_name}" "${git_user_email}"
+  setup_git_config "${token}" "${git_user_name}" "${git_user_email}"
   run_scripts
 
   set +e

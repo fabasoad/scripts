78b229de10de645098bae7171e0fb4391f36a2d1
fabasoad <fabasoad@gmail.com>
2025-10-13 22:19:56 +0900

    fix: use ghpat 3

--- src/automation/main.sh
+++ src/automation/main.sh
@@ -11,6 +11,7 @@ setup_git_config() {
   log_info "Setting up git config started."
   git config user.email "fabasoad@gmail.com"
   git config user.name "fabasoad"
+  git config url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
   log_info "Setting up git config completed."
 }
 
